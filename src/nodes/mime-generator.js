cafde2cd4f2eda10a9b2798d9e481776081f7bac
Victor Almaraz <victor@almaraz.com.br>
2024-01-08 15:37:16 -0300

    Update error handling for node-red 1.x+

--- src/nodes/mime-generator.js
+++ src/nodes/mime-generator.js
@@ -1,53 +1,53 @@
-const { createMimeMessage } =  require("mimetext")
+const { createMimeMessage } = require("mimetext");
 
-module.exports = function(RED) {
-    function MIMEGenerator(config) {
-        RED.nodes.createNode(this,config);
-        var node = this;
-        node.on('input', function(msg) {
-            this.subject = msg.subject ?? config.subject;
-            this.from = msg.from ?? config.from;
-            this.to = msg.to ?? config.to;
-            this.cc = msg.cc ?? config.cc;
-            this.bcc = msg.bcc ?? config.bcc;
+module.exports = function (RED) {
+  function MIMEGenerator(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    node.on("input", function (msg, send, done) {
+      this.subject = msg.subject ?? config.subject;
+      this.from = msg.from ?? config.from;
+      this.to = msg.to ?? config.to;
+      this.cc = msg.cc ?? config.cc;
+      this.bcc = msg.bcc ?? config.bcc;
+      this.replyTo = msg.replyTo ?? config.replyTo;
 
-const email = createMimeMessage();
+      const email = createMimeMessage();
 
-try {
+      try {
+        email.setSender(this.from);
 
-email.setSender(this.from);
+        if (this.subject) email.setSubject(this.subject);
 
-if(this.subject)
-    email.setSubject(this.subject);
+        if (this.to) email.setTo(this.to);
+        if (this.cc) email.setCc(this.cc);
 
+        if (this.bcc) email.setBcc(this.bcc);
 
-if(this.to)
-    email.setTo(this.to);
-if(this.cc)
-    email.setCc(this.cc);
+        if (this.replyTo && this.replyTo != "")
+          email.setHeader("Reply-To", this.replyTo);
 
-if(this.bcc)
-    email.setBcc(this.bcc);
+        if (typeof msg.payload == "string") {
+          email.setMessage("text/plain", msg.payload);
+        } else {
+          const keys = Object.keys(msg.payload);
+          keys.forEach((key, index) => {
+            email.setMessage(key, msg.payload[key]);
+          });
+        }
 
-if(typeof msg.payload == 'string') {
-    email.setMessage('text/plain', msg.payload);
-} else {
-    const keys = Object.keys(msg.payload);
-    keys.forEach((key, index) => {
-        email.setMessage(key, msg.payload[key]);
-    })
-}
-
-msg.payload = email.asRaw();
-this.status({});
-            node.send(msg);
-            
-            
-} catch(err) {
-    this.error(err);
-                    this.status({fill:'red', shape:'ring',text:err});
-}
-        });
-    }
-    RED.nodes.registerType("MIME Generator",MIMEGenerator);
-}
+        msg.payload = email.asRaw();
+        this.status({});
+        node.send(msg);
+      } catch (err) {
+        if (done) {
+          done(err);
+        } else {
+          this.error(err);
+        }
+        this.status({ fill: "red", shape: "ring", text: err });
+      }
+    });
+  }
+  RED.nodes.registerType("MIME Generator", MIMEGenerator);
+};
